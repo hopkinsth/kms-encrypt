a6dd8898f6b7f71dfd39470db23d6f887386073b
Thomas Hopkins <thopkins@redventures.com>
2016-07-19 10:11:01 -0400

    initial code commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kms"
+	"os"
+)
+
+func main() {
+	if len(os.Args < 2) {
+		fmt.Println("You need to supply an ARN and hex value!")
+		return
+	}
+
+	arn := os.Args[0]
+	plhex := os.Args[1]
+
+	if arn == "" {
+		fmt.Println("ARN missing; can't encrypt.")
+		return
+	}
+
+	b, err := hex.DecodeString(plhex)
+	if err != nil {
+		fmt.Printf("Err decoding hex; maybe your value is bad? \n Err was %s\n", err)
+	}
+
+	k := kms.New(session.New())
+	r, err := k.Encrypt(
+		&kms.EncryptInput{
+			Plaintext: b,
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("err encrypting: \n%s\n", err)
+	}
+
+	fmt.Printf("encrypted hex value is %s\n", hex.EncodeToString(r.CiphertextBlob))
+
+}
